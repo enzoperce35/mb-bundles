8be40eb47486001b83394883fd9ff3a4d5418029
Edgar Lorenz G. Castro <elgc@Edgars-iMac.local>
2026-04-22 10:23:33 +0800

    refactored bundle builder

---
+++ src/constants/bundleMenu.js
@@ -0,0 +1,53 @@
+export const HARDCODED_PANTRY = [
+  // Siomai
+  { name: "Siomai", mb_name: "Steamed Siomai Tub Regular", rails_variant_id: 19, pax: 4, count: 12 },
+  { name: "Spicy Siomai", mb_name: "Steamed Siomai Tub Regular", rails_variant_id: 20, pax: 4, count: 12 },
+
+  // Maruya
+  { name: "Maruya Bites", mb_name: "Maruya Bites", rails_variant_id: 14, pax: 5, count: 16 },
+
+  // Chicken Wings
+  { name: "Chicken Wings Honey Soy", mb_name: "Chicken Wings 6pcs Honey Soy", rails_variant_id: 45, pax: 2.5, count: 6 },
+  { name: "Chicken Wings Buffalo", mb_name: "Chicken Wings 6pcs Buffalo", rails_variant_id: 51, pax: 2.5, count: 6 },
+  { name: "Chicken Wings Orange", mb_name: "Chicken Wings 6pcs Orange", rails_variant_id: 52, pax: 2.5, count: 6 },
+  { name: "Chicken Wings Assorted", mb_name: "Chicken Wings 6pcs Assorted", rails_variant_id: 53, pax: 2.5, count: 6 },
+
+  // Empanada
+  { name: "Empanada", mb_name: "Empanada 10pcs", rails_variant_id: 69, pax: 5.5, count: 10 },
+
+  { name: "Chicken Fillet Small", mb_name: "Chicken Fillet Small", rails_variant_id: 83, pax: 8 },
+  { name: "Chicken Fillet Medium", mb_name: "Chicken Fillet Medium", rails_variant_id: 84, pax: 16 },
+  { name: "Chicken Fillet Large", mb_name: "Chicken Fillet Large", rails_variant_id: 85, pax: 24 },
+  { name: "Chicken Fillet XL", mb_name: "Chicken Fillet XL", rails_variant_id: 86, pax: 32 },
+
+  { name: "Shanghai", mb_name: "Shanghai 24pcs", rails_variant_id: 104, pax: 8, count: 24 },
+
+  // Fish Fillet
+  { name: "Fish Fillet Solo", mb_name: "Fish Fillet Solo", rails_variant_id: 182, pax: 3 },
+
+  // Puto
+  { name: "Puto Small", mb_name: "Puto Small 25pcs", rails_variant_id: 41, pax: 12, count: 25 },
+  { name: "Puto Medium", mb_name: "Puto Medium 40pcs", rails_variant_id: 42, pax: 20, count: 40 },
+  { name: "Puto Large", mb_name: "Puto Large 50pcs", rails_variant_id: 43, pax: 25, count: 50 },
+  { name: "Puto XL", mb_name: "Puto XL 75pcs", rails_variant_id: 44, pax: 40, count: 75 },
+
+  // Trays - Carbonara
+  { name: "Carbonara Medium", mb_name: "Carbonara Medium", rails_variant_id: 4, pax: 10, main: true },
+  { name: "Carbonara Large", mb_name: "Carbonara Large", rails_variant_id: 5, pax: 15, main: true },
+  { name: "Carbonara XL", mb_name: "Carbonara XL", rails_variant_id: 6, pax: 20, main: true },
+
+  // Trays - Spaghetti
+  { name: "Spaghetti Medium", mb_name: "Spaghetti Medium", rails_variant_id: 1, pax: 10, main: true },
+  { name: "Spaghetti Large", mb_name: "Spaghetti Large", rails_variant_id: 2, pax: 15, main: true },
+  { name: "Spaghetti XL", mb_name: "Spaghetti XL", rails_variant_id: 3, pax: 20, main: true },
+
+  // Trays - Palabok
+  { name: "Palabok Medium", mb_name: "Palabok Medium", rails_variant_id: 7, pax: 10, main: true },
+  { name: "Palabok Large", mb_name: "Palabok Large", rails_variant_id: 8, pax: 15, main: true },
+  { name: "Palabok XL", mb_name: "Palabok XL", rails_variant_id: 9, pax: 20, main: true },
+
+  // Trays - Pancit Bihon
+  { name: "Pancit Bihon Medium", mb_name: "Pancit Bihon Medium", rails_variant_id: 10, pax: 10, main: true },
+  { name: "Pancit Bihon Large", mb_name: "Pancit Bihon Large", rails_variant_id: 11, pax: 15, main: true },
+  { name: "Pancit Bihon XL", mb_name: "Pancit Bihon XL", rails_variant_id: 12, pax: 20, main: true },
+];
