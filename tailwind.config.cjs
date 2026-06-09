53a3326c66ac4f83d0b678bffdcf708ff46c5121
Edgar Lorenz G. Castro <elgc@Edgars-iMac.local>
2026-06-09 20:54:42 +0800

    setup tailwind version 3.4

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,7 +9,6 @@ export default {
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
       },
-      // Adding the floating animation logic here
       keyframes: {
         float: {
           '0%, 100%': { transform: 'translateY(0)' },
@@ -19,7 +18,6 @@ export default {
       animation: {
         float: 'float 4s ease-in-out infinite',
       },
-      // Adding a custom drop shadow for the logo
       dropShadow: {
         'logo': '0 10px 10px rgba(0, 0, 0, 0.5)',
       }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Add this import
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Add this to your plugins array
   ],
 })
